be9d5dcb5e7d56af02ca59cb7e1e5cb3f1eb317e
Abolfazl <boorbooryabolfazl85@gmail.com>
2022-01-23 07:24:24 -0800

    Change of modal and button add to cart

--- scripts/index.js
+++ scripts/index.js
@@ -28,12 +28,13 @@ const hiddenModal = () => {
   modal.classList.add("hidden");
 };
 
-const removeItemCart = (id, price) => {
+const removeItemCart = (id, price, textBtn) => {
   const idItem = document.getElementById(id);
   const priceItem = price;
   totalPrice.innerHTML = Number(totalPrice.innerHTML) - priceItem;
   cartNumberItems.innerHTML--;
   idItem.remove();
+  textBtn.innerHTML = "add to cart";
 };
 
 btnsAddToCart.forEach((btn) => {
@@ -54,7 +55,7 @@ btnsAddToCart.forEach((btn) => {
               <p>${selectedProduct.price}</p>
           </div>
           <div class="btn-Remove">
-              <button onclick="removeItemCart(${selectedProduct.id},${selectedProduct.price})">Delete</button>
+              <button onclick="removeItemCart(${selectedProduct.id},${selectedProduct.price},${button})">Delete</button>
           </div>
       </div>
     `;
@@ -65,6 +66,7 @@ btnsAddToCart.forEach((btn) => {
       for (const item of allItems) {
         item.remove();
       }
+      btn.textContent = "add to cart";
       totalPrice.innerHTML = 0;
       cartNumberItems.innerHTML = 0;
     });
